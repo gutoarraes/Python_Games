17635982eddea5cbda81349c390f6391cd54dae1
Augusto Arraes <augustobarraes@gmail.com>
2022-05-18 19:48:10 -0400

    Update apple_game.py
    Changed image pathing to relative path

--- Apple game/apple_game.py
+++ Apple game/apple_game.py
@@ -1,43 +1,40 @@
-
-import pygame
-from pygame.locals import *
-import random
+# Imports
 from os import path
+import random
+from re import X
 import sys
+from turtle import color
+import pygame
 
 
 pygame.init()
-
-# settings
+# Settings
 size = width, height = (1600, 1000)
 FPS = 60
-running = True
 BLACK = (0,0,0)
-blue = (0,0,255)
-GREEN = (0,255,0)
-RED = (255,0,0)
 WHITE = (255,255,255)
-font_name = pygame.font.match_font("comicsansms")
-click = False
+BLUE = (0,0,255)
+font_name = pygame.font.match_font("comicsans")
+font_size = 25
 
-# Game window
-FpsClock = pygame.time.Clock() 
+
+# Game Window
+FpsClock = pygame.time.Clock()
 screen = pygame.display.set_mode(size, 0, 32)
-pygame.display.set_caption("Fruit game")
-pygame.display.update()
+pygame.display.set_caption("Apple game")
 
 
 # Images
-apple = pygame.image.load("apple2.png").convert()
+game_folder = path.dirname(__file__)
+apple = pygame.image.load(path.join(game_folder, "apple2.png")).convert()
 apple = pygame.transform.rotozoom(apple, 0, 0.08)
-apple.set_colorkey((0,0,0))
-background = pygame.image.load("forest.jpg").convert()
-background = pygame.transform.scale(background,size)
+apple.set_colorkey(BLACK)
+background = pygame.image.load(path.join(game_folder, "forest.jpg")).convert()
+background = pygame.transform.scale(background, size)
 background_rect = background.get_rect()
-screen.blit(background,(0,0))
 
 
-# Apples
+# Apple class
 class Apple(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -45,19 +42,26 @@ class Apple(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, width - self.rect.width)
         self.rect.y = -50
-        self.speed_y = random.randrange(1, 8)
-    
+        self.speed_y = random.randrange(1, 10)
+
     def update(self):
         self.rect.y += self.speed_y
 
-
 # Group all apples
-all_apples = pygame.sprite.Group() 
+all_apples = pygame.sprite.Group()
+
+# Global variables
+class globalVariables():
+    score = 0
+    lives = 3
+
+for i in range(1, 5):
+    a = Apple()
+    all_apples.add(a)
 
 
-# Display to screen
-def message_to_screen(message, color, font_size, x, y):
-    """Displays messages to screen"""
+# Display text to screen
+def screenText(message, color, font_size, x, y):
     font = pygame.font.SysFont(font_name, font_size)
     text = font.render(message, True, color)
     text_rect = text.get_rect()
@@ -65,105 +69,81 @@ def message_to_screen(message, color, font_size, x, y):
     screen.blit(text, text_rect)
 
 
-for i in range(1, 5):
-    a = Apple()
-    all_apples.add(a)
 
-
-class globaVariables():
-    score = 0
-    lives = 3
-
+# Main game
 def game():
-    
 
     running = True
     while running:
-        
-        mx, my = pygame.mouse.get_pos()
-        # Draw to screen   
-        screen.blit(background,(0,0))
-        all_apples.draw(screen) 
-        all_apples.update()
-        message_to_screen("Your score is: " + str(globaVariables.score), WHITE, 24, width*0.05, height*0.05)
-        message_to_screen(str(globaVariables.lives) + " lives", WHITE, 24, width*0.03, height*0.03)
-
-        click = False
-
-        # End game if player presses window "X"
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-            
         pygame.display.update()
+        # get mouse position
+        mx, my = pygame.mouse.get_pos()
+        
+        #Draw to screen
+        screen.blit(background,(0,0))
+        screenText("Score: " + str(globalVariables.score), WHITE, 25, width*0.05, height*0.05)
+        screenText("lives: " + str(globalVariables.lives), WHITE, 25, width*0.05, height*0.08)
+        all_apples.draw(screen)
+        all_apples.update()        
+
+        # check if player clicks the x button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit
+        
         FpsClock.tick(FPS)
         if len(all_apples) < 4:
             a = Apple()
             all_apples.add(a)
 
+        # Kill apple player clicks it (compare mouse position with apple position, check if user left click, if so kill Apple)
         if pygame.mouse.get_pressed()[0]:
             for item in all_apples:
-                    if item.rect.collidepoint((mx, my)):  
-                        item.kill()
-                        globaVariables.score = globaVariables.score + 1
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    click = True
+                if item.rect.collidepoint((mx,my)):
+                    item.kill()
+                    globalVariables.score += 1
                     
-        
+        # If apple falls below screen, kill Apple and deduct 1 life
         for item in all_apples:
             if item.rect.y > height:
-                globaVariables.lives -= 1
-                screen.blit(background,(0,0))
                 item.kill()
-                if globaVariables.lives == 0:
+                globalVariables.lives -= 1
+                if globalVariables.lives == 0:
                     end_game()
-                    
 
+
+# End game screen
 def end_game():
+    
+    mx, my = pygame.mouse.get_pos()
     running = True
     while running:
-        pygame.draw.rect(screen, WHITE, (width*0.4, height*0.4, 330, 250))
-        message_to_screen("Total score: " + str(globaVariables.score), BLACK, 50, width/2, height/2)
-        #restart = message_to_screen("Restart? ", GREEN, 50, 800, 600)
-
-        text = pygame.font.SysFont(font_name, 50).render("Restart?", True, GREEN)
-        text_rect = text.get_rect()
-        text_rect.center = (800, 600)
-        screen.blit(text, text_rect)
         pygame.display.update()
-        FpsClock.tick(60)
+        screen.blit(background,(0,0))
+        pygame.draw.rect(screen, WHITE, (width*0.4, height*0.4, 330, 250))
+        screenText("Total Score " + str(globalVariables.score), BLACK, 50, width/2, height/2)
+        # screenText("Restart?", BLUE, 50, 800, 550)
+        text = pygame.font.SysFont(font_name, 50).render("Restart?", True, BLUE)
+        text_rect = text.get_rect()
+        text_rect.center = (800, 550)
+        screen.blit(text, text_rect)
+    
 
-        click = False
-
+        # check if player clicks the x button
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 pygame.quit()
-                sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    click = True
-            if pygame.mouse.get_pressed()[0]:
-                if text_rect.collidepoint(event.pos):
-                    globaVariables.lives = 3
-                    globaVariables.score = 0 
-                    for item in all_apples:
-                        item.kill()
-                    for i in range(1, 5):
-                        a = Apple()
-                        all_apples.add(a)
-                    game()
+                sys.exit
+        
+        # check if player clicks restart, in which case start game again
+        if pygame.mouse.get_pressed()[0]:
+            if text_rect.collidepoint(event.pos):
+                # reset variables
+                globalVariables.lives = 3
+                globalVariables.score = 0
+                game()            
+                
 
 game()
 
